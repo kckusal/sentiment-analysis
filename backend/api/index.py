03e82f318f43a2f6ca8dc276a59139a269f255cc
Kusal K C <kusal.kc@student.torrens.edu.au>
2023-11-27 08:04:21 +1000

    feat: api endpoint to evaluate sentiment

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -9,3 +9,12 @@ def home():
 @app.route('/about')
 def about():
   return 'About'
+
+@app.post('/api/evaluate-sentiment')
+def evaluate_sentiment():
+  body = request.get_json()
+  
+  if 'input_text' in body:
+    return "Got body: {}".format(request.get_json())
+  else:
+    return 'Bad request: The HTTP request must have the Content-Type: "application/json" and contain "input_text" param in body.', 400
