895345fadfc1ee58f46190824b6a974d70cdc209
Kusal K C <kusal.kc@student.torrens.edu.au>
2023-12-10 12:50:48 +1000

    refactor: structure

--- backend/api.py
+++ backend/api.py
@@ -30,7 +30,7 @@ def evaluate_sentiment():
   
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=port)
+  app.run(host='0.0.0.0', port=port, debug=True)
 
 def create_app():
   print('PORT is {}'.format(port))
